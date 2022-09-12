ac49570f9bfdb09b206a5dcc4d25e9faa123ac82
poiintt <abendmo@gmail.com>
2022-09-12 22:19:23 +0200

    feat: query multiple query params
    - add .env
    - create go module
    - configure debug profile
    - add pdfs folder

---
+++ go.mod
@@ -0,0 +1,5 @@
+module poiintt/find-sports-certificate
+
+go 1.17
+
+require github.com/joho/godotenv v1.4.0
--- main.go
+++ main.go
@@ -7,49 +7,63 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"strings"
+	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
-// stolen from https://golangdocs.com/golang-download-files
-
 var (
-	fileName    string
-	fullURLFile string
+	fileName string
+	pdfUrl   string
 )
 
 func main() {
-	fullURLFile = os.Getenv("URL")
+	err := godotenv.Load(".env")
 
-	// Build fileName from fullPath
-	fileURL, err := url.Parse(fullURLFile)
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	pdfUrl = os.Getenv("URL")
+	fileURL, err := url.Parse(pdfUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
-	path := fileURL.Path
-	segments := strings.Split(path, "/")
-	fileName = segments[len(segments)-1]
+	values := fileURL.Query()
 
-	// Create blank file
-	file, err := os.Create(fileName)
-	if err != nil {
-		log.Fatal(err)
+	for i := 0; i <= 10; i++ {
+		values.Set("ID", strconv.Itoa(i))
+		fileURL.RawQuery = values.Encode()
+		fmt.Printf("%s", fileURL.String())
+		fmt.Println()
+
+		fileName = strconv.Itoa(i) + ".pdf"
+
+		// Create blank file
+		file, err := os.Create("./pdfs/" + fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		client := http.Client{
+			CheckRedirect: func(r *http.Request, via []*http.Request) error {
+				r.URL.Opaque = r.URL.Path
+				return nil
+			},
+		}
+		// Put content on file
+		resp, err := client.Get(fileURL.String())
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		size, err := io.Copy(file, resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		fmt.Printf("Downloaded a file %s with size %d", fileName, size)
+		fmt.Println()
 	}
-	client := http.Client{
-		CheckRedirect: func(r *http.Request, via []*http.Request) error {
-			r.URL.Opaque = r.URL.Path
-			return nil
-		},
-	}
-	// Put content on file
-	resp, err := client.Get(fullURLFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	size, err := io.Copy(file, resp.Body)
-
-	defer file.Close()
-
-	fmt.Printf("Downloaded a file %s with size %d", fileName, size)
 }
