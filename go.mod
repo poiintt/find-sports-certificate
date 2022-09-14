7e4d6f2312a4c7629523dcc75b35cdcbafe6615a
poiintt <abendmo@gmail.com>
2022-09-14 21:54:40 +0200

    feat: save result in sqlite db

--- go.mod
+++ go.mod
@@ -9,16 +9,14 @@ require (
 
 require (
 	github.com/docker/go-units v0.5.0
+	github.com/mattn/go-sqlite3 v1.14.15
 	github.com/unidoc/unipdf/v3 v3.38.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
-	github.com/paulrosania/go-charset v0.0.0-20190326053356-55c9d7a5834c
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca
-	github.com/sirupsen/logrus v1.5.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/stretchr/testify v1.7.1 // indirect
 	github.com/unidoc/pkcs7 v0.1.0 // indirect
 	github.com/unidoc/timestamp v0.0.0-20200412005513-91597fd3793a // indirect
@@ -28,6 +26,6 @@ require (
 	golang.org/x/net v0.0.0-20220909164309-bea034e7d591
 	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"database/sql"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -16,6 +17,7 @@ import (
 	"github.com/docker/go-units"
 	"github.com/joho/godotenv"
 	"github.com/ledongthuc/pdf"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/unidoc/unipdf/v3/model"
 	"golang.org/x/net/html/charset"
 )
@@ -39,6 +41,9 @@ func downloadPdfs(start int, end int) {
 
 	pdfFolder := "./pdfs/"
 
+	db, _ := sql.Open("sqlite3", "./sqlite.db")
+	defer db.Close()
+
 	for i := start; i <= end; i++ {
 		start := time.Now()
 
@@ -49,57 +54,69 @@ func downloadPdfs(start int, end int) {
 
 		// skip downloaded files
 		if file != nil {
-			fmt.Printf("Skipping")
+			fmt.Println("Found PDF")
 		} else {
-			urlValues.Set("ID", strconv.Itoa(i))
-			fileURL.RawQuery = urlValues.Encode()
 
-			client := http.Client{
-				CheckRedirect: func(r *http.Request, via []*http.Request) error {
-					r.URL.Opaque = r.URL.Path
-					return nil
-				},
+			cert := getCertificate(db, i)
+			if cert != 0 {
+				fmt.Println("Found db entry")
+			} else {
+				urlValues.Set("ID", strconv.Itoa(i))
+				fileURL.RawQuery = urlValues.Encode()
+
+				fmt.Println("Request")
+				resp, err := http.Get(fileURL.String())
+
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer resp.Body.Close()
+
+				duration := time.Since(start)
+				fmt.Printf("Downloaded in %s", duration)
+				fmt.Println()
+				contentType := resp.Header["Content-Type"][0]
+
+				sql := `INSERT INTO certificate(id, type) VALUES(?, ?)
+  ON CONFLICT(id) DO UPDATE SET type=excluded.type;`
+				statement, _ := db.Prepare(sql)
+				statement.Exec(i, contentType)
+
+				if contentType == "application/pdf" {
+					// create empty file
+					file, err = os.Create(pdfFolder + fileName)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					size, err := io.Copy(file, resp.Body)
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer file.Close()
+
+					fileSize := units.HumanSize(float64(size))
+					fmt.Printf("(%s)", fileSize)
+					fmt.Println()
+				} else {
+					fmt.Println(contentType)
+				}
 			}
-
-			resp, err := client.Get(fileURL.String())
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer resp.Body.Close()
-
-			// create empty file
-			file, err := os.Create(pdfFolder + fileName)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			size, err := io.Copy(file, resp.Body)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer file.Close()
-
-			fileSize := units.HumanSize(float64(size))
-			fmt.Printf("Downloaded (%s)", fileSize)
 		}
 
 		content, err := getPdfContent(pdfFolder + fileName)
-		if err != nil {
-			panic(err)
+		if err == nil {
+
+			subject, err := getPdfSubject(file)
+			if err != nil {
+				panic(err)
+			}
+			name := strings.Replace(subject, "Urkunde ", "", 1)
+			content = strings.Replace(content, name, "", 1)
+
+			insertCertificate(db, i, name, content)
+			getCertificate(db, i)
 		}
-
-		subject, err := getPdfSubject(file)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println()
-		fmt.Println("subject: ", subject)
-		fmt.Println("content: ", content)
-
-		duration := time.Since(start)
-		fmt.Printf(" in %s", duration)
-		fmt.Println()
 	}
 }
 
@@ -108,17 +125,12 @@ func getPdfContent(path string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	totalPage := r.NumPage()
 
 	var textBuilder bytes.Buffer
-	for pageIndex := 1; pageIndex <= totalPage; pageIndex++ {
-		p := r.Page(pageIndex)
-		if p.V.IsNull() {
-			continue
-		}
-		s, _ := p.GetPlainText(nil)
-		textBuilder.WriteString(s)
-	}
+	p := r.Page(1)
+
+	s, _ := p.GetPlainText(nil)
+	textBuilder.WriteString(s)
 	return textBuilder.String(), nil
 }
 
@@ -127,7 +139,6 @@ func getPdfSubject(file *os.File) (string, error) {
 
 	pdfInfo, _ := pdfReader.GetPdfInfo()
 	subject := pdfInfo.Subject.String()
-	subject = strings.Replace(subject, "Urkunde ", "", 1)
 	subject = convertToUTF8(subject, "ascii")
 
 	return subject, nil
@@ -139,4 +150,61 @@ func convertToUTF8(str string, origEncoding string) string {
 	reader, _ := charset.NewReaderLabel(origEncoding, byteReader)
 	strBytes, _ = ioutil.ReadAll(reader)
 	return string(strBytes)
+}
+
+func initDb() {
+	fmt.Println("Creating sqlite.db...")
+	file, err := os.Create("sqlite.db")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	file.Close()
+	fmt.Println("sqlite.db created")
+
+	db, _ := sql.Open("sqlite3", "./sqlite.db")
+	defer db.Close()
+	createTable(db)
+}
+
+func createTable(db *sql.DB) {
+	sql := `CREATE TABLE certificate (
+		"id" integer NOT NULL PRIMARY KEY,		
+		"name" TEXT,
+		"content" TEXT
+		"type" TEXT
+	  );`
+
+	fmt.Println("Create certificate table...")
+	statement, err := db.Prepare(sql)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	statement.Exec()
+	fmt.Println("certificate table created")
+}
+
+func insertCertificate(db *sql.DB, id int, name string, content string) {
+	sql := `INSERT INTO certificate(id, name, content) VALUES(?, ?, ?)
+  ON CONFLICT(id) DO UPDATE SET name=excluded.name, content=excluded.content;`
+
+	statement, err := db.Prepare(sql)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	_, err = statement.Exec(id, name, content)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func getCertificate(db *sql.DB, id int) int {
+	sql := "SELECT id FROM certificate WHERE id = (?)"
+
+	var id2 int
+
+	err := db.QueryRow(sql, id).Scan(&id2)
+	if err != nil {
+		return 0
+	}
+	return 1
 }
